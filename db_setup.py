3f828a73d4af758521213d81d2bbfd27bb49f8b7
chmcbs <151023884+chmcbs@users.noreply.github.com>
2026-04-27 12:52:42 +0100

    feat: add postgres setup script

---
+++ db_setup.py
@@ -0,0 +1,34 @@
+"""
+Creates postgres table for storing fetched papers
+
+"""
+
+import psycopg
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+CREATE_PAPERS_TABLE = """
+CREATE TABLE IF NOT EXISTS papers (
+    arxiv_id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    abstract TEXT,
+    authors TEXT[],
+    published_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    pdf_url TEXT,
+    entry_url TEXT,
+    categories TEXT[],
+    inserted_at TIMESTAMPTZ DEFAULT NOW()
+);
+"""
+
+def main():
+    with psycopg.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute(CREATE_PAPERS_TABLE)
+
+if __name__ == "__main__":
+    main()
