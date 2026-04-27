52e383f65fff24cae7e0d86a01273f720c9e2997
chmcbs <151023884+chmcbs@users.noreply.github.com>
2026-04-27 09:38:12 +0100

    Initial commit

---
+++ ingestion.py
@@ -0,0 +1,24 @@
+"""
+Fetches papers from arXiv
+
+"""
+
+import arxiv
+
+def fetch_papers(
+    category: str = 'cs.AI',
+    max_results: int = 10,
+    sort_by: arxiv.SortCriterion = arxiv.SortCriterion.SubmittedDate,
+    sort_order: arxiv.SortOrder = arxiv.SortOrder.Descending,
+    ):
+    client = arxiv.Client()
+    search = arxiv.Search(
+        query=f"cat:{category}",
+        max_results=max_results,
+        sort_by=sort_by,
+        sort_order=sort_order,
+    )
+    papers = client.results(search)
+    papers_list = list(papers)
+
+    return papers_list
